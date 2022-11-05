592c28da318fc2ed8fa4286d0954c09feff03589
acandia <abelcandiach@gmail.com>
2022-11-05 20:36:26 -0300

    terminando CRUD agregando algunos estilos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   end
 
   def articles
-    @articles = Article.all
+    @articles = Article.order(:articles).page(params[:page])
   end
   
   def new
@@ -12,12 +12,40 @@ class PagesController < ApplicationController
   end
   
   def create
-    data_article = params.require(:article).permit(:title, :content)
-    @article = Article.new(data_article)
+    @article = Article.new(article_params)
     if @article.save
         redirect_to articles_path
     else
         render :new
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path, notice: 'Article was successfully updated'
+    else
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  def delete
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, notice: 'Article was destroy successfully'
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get '/index', to: 'pages#index'
+  get '/index',             to: 'pages#index'
   root 'pages#articles'
 
-  get 'articles', to: 'pages#articles'
-  get '/article/new', to: 'pages#new'
-  post '/articles', to: 'pages#create'
+  get '/articles',          to: 'pages#articles'
+  get '/articles/new',      to: 'pages#new'
+  post '/articles',         to: 'pages#create'
+
+  delete '/articles/:id',   to: 'pages#delete'
+  patch '/articles/:id',    to: 'pages#update'
+  get '/articles/:id',      to: 'pages#show', as: :article
+  get '/articles/:id/edit', to: 'pages#edit', as: :edit_article
+
 end
